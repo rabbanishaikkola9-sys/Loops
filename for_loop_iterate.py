cacb02e31476fbfe3c8d8a80efd14f0774b40eb0
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-04-30 21:56:21 +0530

    added

---
+++ for_loop_iterate.py
@@ -0,0 +1,11 @@
+l=[1,2,3,'rabbani',True]
+for i in l:
+    print(i)
+## Using tuple
+t=(1,2,3,'rabbani',True)
+for i in t:
+    print(i)
+## Using string
+name=input("enter the user name:\n")
+for i in name:
+    print(i)
---
+++ forwithelse.py
@@ -0,0 +1,4 @@
+for i in range(0,10):
+    print("The value of i is :",i)
+else:
+    print("The for loop is done ...\n")
