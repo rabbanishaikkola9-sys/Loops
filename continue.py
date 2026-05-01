4b3e18b0711b86d855172147ee27da6b8221d667
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-05-01 09:42:29 +0530

    added

---
+++ continue.py
@@ -0,0 +1,5 @@
+for i in range(10):
+    if(i==10):
+        continue
+    else:
+        print("the value of i is :\t",i)
--- forwithelse.py
+++ forwithelse.py
@@ -1,4 +1,4 @@
 for i in range(0,10):
     print("The value of i is :",i)
 else:
-    print("The for loop is done ...\n")
+    print("The for loop is done ...\n")w
